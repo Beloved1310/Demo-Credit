35447f10724dd18cc82e8dd82827de5ab9a26d33
Beloved1310 <adeoluwafisayomi@gmail.com>
2022-12-02 09:38:18 +0100

    feat: refactor code

--- route/user.js
+++ route/user.js
@@ -1,26 +1,21 @@
 /* eslint consistent-return: "off" */
 
-const express = require('express');
-const asyncMiddleware = require('../middleware/async');
-const { authJWT } = require('../middleware/auth');
-
+const express = require("express");
+const asyncMiddleware = require("../middleware/async");
+const { authJWT } = require("../middleware/auth");
 
 const router = express.Router();
 
-const signUp = require('../controller/users/signUp');
-const loginUser = require('../controller/users/loginUser');
-const fundUser = require('../controller/fundWallet');
-const fundAccount = require('../controller/fundAnotherAccount');
-const withdrawfunds = require('../controller/withdrawfunds');
+const signUp = require("../controller/users/signUp");
+const loginUser = require("../controller/users/loginUser");
+const fundUser = require("../controller/fundWallet");
+const fundAccount = require("../controller/fundAnotherAccount");
+const withdrawfunds = require("../controller/withdrawfunds");
 
-
-
-router.post('/signup', asyncMiddleware(signUp));
-router.post('/login', asyncMiddleware(loginUser));
-router.post('/fund', authJWT, asyncMiddleware(fundUser));
-router.post('/fundaccount', authJWT, asyncMiddleware(fundAccount));
-router.post('/withdrawal', authJWT, asyncMiddleware(withdrawfunds));
-
-
+router.post("/signup", asyncMiddleware(signUp));
+router.post("/login", asyncMiddleware(loginUser));
+router.post("/fund", authJWT, asyncMiddleware(fundUser));
+router.post("/fundaccount", authJWT, asyncMiddleware(fundAccount));
+router.post("/withdrawal", authJWT, asyncMiddleware(withdrawfunds));
 
 module.exports = router;
