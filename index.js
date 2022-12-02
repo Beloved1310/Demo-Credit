d26484b9b042508b3bedcc86a2f30c363185e87f
Beloved1310 <adeoluwafisayomi@gmail.com>
2022-12-02 11:24:01 +0100

    feat: refactor code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const debug = require('debug')('app');
-const { PORT } = require('./config');
-const app = require('./app')
-
+const debug = require("debug")("app");
+const { PORT } = require("./config");
+const app = require("./app");
 
 app.listen(PORT, () => {
   debug(`Web server is running ${PORT}`);
